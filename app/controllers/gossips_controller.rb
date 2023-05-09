5dfd0190bd6450fbbf9caf1a1e7e1c92abac6291
Camille Lenormand <camille.lenormand02@gmail.com>
2023-05-09 16:29:01 +0200

    add redirections when gossip saved

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -8,7 +8,17 @@ class GossipsController < ApplicationController
     @id = params[:id]
   end
 
+  def new
+    @gossip= Gossip.new
+  end
+
   def create 
     @gossip = Gossip.new(title: params[:title], content: params[:content], user_id: rand(1..100))
+    
+    if @gossip.save
+      redirect_to gossips_path, success: "Gossip successfully created !"
+    else
+      redirect_to gossips_path, danger: "Error, gossip not created !"
+    end
   end
 end
