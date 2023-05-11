ef80a542c9bd39e6b14dfcd14144b5b4dc002bc6
Camille Lenormand <camille.lenormand02@gmail.com>
2023-05-11 11:29:37 +0200

    add create method and user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,33 @@
 class UsersController < ApplicationController
+  include SessionsHelper
+
+
   def show
     @id = params[:id].to_i
   end
+
+  def create
+    @user = User.new(
+      email: user_params[:email], 
+      password: user_params[:password],
+      password_confirmation: user_params[:password_confirmation],
+    )
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(
+    :username,
+    :first_name, 
+    :last_name, 
+    :age, 
+    :description, 
+    :city_id,
+    :email,
+    :password,
+    :password_confirmation
+    )
+  end
+
 end
