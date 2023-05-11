d9d6adc4b840e44ad2980dba9405208bda05193b
Camille Lenormand <camille.lenormand02@gmail.com>
2023-05-11 09:29:27 +0200

    add password and username to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_05_08_093531) do
+ActiveRecord::Schema[7.1].define(version: 2023_05_11_072033) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -95,6 +95,9 @@ ActiveRecord::Schema[7.1].define(version: 2023_05_08_093531) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "city_id"
+    t.string "password_digest"
+    t.string "username"
+    t.string "full_name"
     t.index ["city_id"], name: "index_users_on_city_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,6 @@
 require 'faker'
 
 
-100.times do
-  users = User.create!([{ first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, age: Faker::Number.between(from: 7, to: 77), description: Faker::Lorem.sentences(number: 1), email: Faker::Internet.email }])
-end
-
-puts "Users created!"
-
 100.times do 
 cities = City.create!([{ name: Faker::Address.city, zip_code: Faker::Address.zip_code }])
 end
@@ -21,13 +15,19 @@ end
 puts "Cities created!"
 
 100.times do
-    gossips = Gossip.create!([{ title: Faker::Lorem.sentences, content: Faker::Lorem.sentences(number: 3), user_id: Faker::Number.between(from: 1, to: 100) }])
+  users = User.create!([{ first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, age: Faker::Number.between(from: 7, to: 77), description: Faker::Lorem.sentences(number: 1), email: Faker::Internet.email, city_id: Faker::Number.between(from: 1, to: 100) }])
+end
+
+puts "Users created!"
+
+100.times do
+    gossips = Gossip.create!([{ title: Faker::Lorem.characters(number: 10), content: Faker::Lorem.sentences(number: 1), user_id: Faker::Number.between(from: 1, to: 100) }])
 end
 
 puts "Gossips created!"
 
 100.times do
-  tags = Tag.create!([{ title: Faker::Lorem.sentences }])
+  tags = Tag.create!([{ title: Faker::Lorem.characters(number: 10) }])
 end
 
 puts "Tags created!"
