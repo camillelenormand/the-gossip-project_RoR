dec91bf25fdb82479b510c092ea373fef4321aea
Camille Lenormand <camille.lenormand02@gmail.com>
2023-05-11 10:07:25 +0200

    create session controller with all CRUD methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,25 @@
 class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    # find existing user
+    user = User.find_by(email: params[:email])
+
+    # check if user exists and password is correct
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to gossips_path, success: "Logged in!"
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render :new
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+  end
+
 end
