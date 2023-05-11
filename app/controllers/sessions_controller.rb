e5a7edf93d04cc6e43f070992af61677e25415ba
Camille Lenormand <camille.lenormand02@gmail.com>
2023-05-11 16:18:12 +0200

    update session methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,29 +1,37 @@
 class SessionsController < ApplicationController
+  skip_before_action :authorized, only: [:new, :create, :welcome]
+
 
   # GET /sessions/new
   def new
     @user = User.new
   end
 
+  def login
+    
+  end
+
   # POST /sessions
   def create
-    # find existing user
-    user = User.find_by(email: params[:email])
-
-    # check if user exists and password is correct
-    if user && user.authenticate(params[:password])
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to gossips_path, success: "Logged in!"
+      redirect_to '/welcome'
     else
-      flash.now[:danger] = 'Invalid email/password combination'
-      render :new
+      redirect_to '/login'
     end
   end
 
   # delete /sessions
   def destroy
-    session.delete(:user_id)
+    session.delete(:current_user_id)
+    # Clear the memoized current user
+    @_current_user = nil
     redirect_to new_session_path, success: "Logged out!"
+  end
+
+  def page_requires_login
+
   end
 
 end
