4a1b93abc54f578038385dbf365b3c50cc95fd45
Camille Lenormand <camille.lenormand02@gmail.com>
2023-05-11 16:16:58 +0200

    add current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   add_flash_types :danger, :info, :warning, :success
+  helper_method :current_user, :logged_in?
+  before_action :authorized
+
+  def current_user
+    User.find_by(id: session[:current_user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def authorized
+    redirect_to '/welcome' unless logged_in?
+  end
 end
