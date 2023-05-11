5f30d1752600f5a09d112fe3b4d6cd746e7a7d63
Camille Lenormand <camille.lenormand02@gmail.com>
2023-05-11 09:32:49 +0200

    add update method in gossip controller

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -14,13 +14,38 @@ class GossipsController < ApplicationController
 
   def create 
     @gossip = Gossip.new(title: params[:title], content: params[:content], user_id: rand(1..100))
-    
-    if @gossip.save
-      redirect_to gossips_path, success: "Gossip successfully created !"
-    else
-      @gossip.errors.full_messages.each do |message|
-      redirect_to new_gossip_path, danger: message
-    end
+      
+      if @gossip.save
+        redirect_to gossips_path, success: "Gossip successfully created !"
+      else
+        @gossip.errors.full_messages.each do |message|
+        redirect_to new_gossip_path, danger: message
+      end
     end
   end
+
+  def update
+    @gossip = Gossip.find(params[:id])
+  
+    if @gossip.update(title: params[:title], content: params[:content], user_id: @gossip.user_id)
+      redirect_to gossips_path, success: "Gossip successfully updated !"
+    else
+      @gossip.errors.full_messages.each do |message|
+      redirect_to edit_gossip_path(@gossip), danger: message
+      end
+      render :edit
+    end
+  end
+
+  def destroy 
+    @gossip = Gossip.find(params[:id])
+    @gossip.destroy
+    
+    if @gossip.destroy
+      redirect_to gossips_path, success: "Gossip successfully deleted !"
+    else
+      redirect_to gossips_path, danger: "Gossip not deleted !"
+    end
+  end
+
 end
