bd68f1fdf2367f2aff17b32d4cd628ddeb676081
Camille Lenormand <camille.lenormand02@gmail.com>
2023-05-11 11:30:02 +0200

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,23 @@ class User < ApplicationRecord
   validates :email, presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Email address invalid" }
   validates_uniqueness_of :email, :case_sensitive => false
   validates :password, presence: true, length: { minimum: 6 }
+
+  before_update :set_full_name
+  before_create :set_full_name
+  before_create :get_username
+
+  private
+  def set_full_name
+    if self.first_name.nil? && self.last_name.nil?
+      self.full_name = self.email.split('@')[0].capitalize
+    else 
+      self.full_name = self.first_name.capitalize + " " + self.last_name.capitalize
+    end
+  end
+
+  def get_username
+    if self.username.nil? || self.username.length < 1
+    self.username = self.email.split('@')[0].lowercase
+    end
+  end
 end
