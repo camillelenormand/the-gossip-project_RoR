bec763e9a46eb8a13aaa689ee36d278c1477ee32
Camille Lenormand <camille.lenormand02@gmail.com>
2023-05-11 09:33:05 +0200

    add city controller

---
+++ app/controllers/cities_controller.rb
@@ -0,0 +1,7 @@
+class CitiesController < ApplicationController
+  def show
+    @id = City.find(params[:id])
+    @cities_users = User.where(city_id: @id)
+    @gossips = Gossip.where(user_id: @cities_users)
+  end
+end
