ef00322124130e60c9569b97b39967aeb1afd4da
Camille Lenormand <camille.lenormand02@gmail.com>
2023-05-11 16:19:07 +0200

    check if user is logged in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,10 +1,17 @@
 module SessionsHelper
   def current_user
     User.find_by(id: session[:user_id])
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
   end
 
   def log_in(user)
     session[:user_id] = user.id # store user id in session
+  end
+
+  def logged_in?
+    session[:user_id].present?
   end
 
   def log_out
