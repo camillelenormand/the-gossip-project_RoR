ba084463fdc81f2a2340b9ee06c073df48abb3f0
Camille Lenormand <camille.lenormand02@gmail.com>
2023-05-11 09:35:35 +0200

    update routes with new urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,24 @@
 Rails.application.routes.draw do  
   
   ### gossips ###
-  resources :gossips
+  resources :gossips do
+    resources :comments
+  end
+
+  ## likes ##
+  resources :likes, only: [:new, :create, :destroy]
+
+  ### sessions ###
+  resources :sessions, only: [:new, :create, :destroy]
 
   ### users ###
   resources :users
+
+  ### comments ###
+  resources :comments
+
+  ### cities ###
+  resources :cities
 
   ## static pages ##
   get '/', to: 'gossips#index'
