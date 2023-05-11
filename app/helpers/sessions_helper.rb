67eb9423c539340403f54dceb9adb806c0790641
Camille Lenormand <camille.lenormand02@gmail.com>
2023-05-11 10:07:45 +0200

    Add session helper to handle current user, login and logout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,14 @@
 module SessionsHelper
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def log_in(user)
+    session[:user_id] = user.id # store user id in session
+  end
+
+  def log_out
+    session.delete(:user_id) # delete user id from session
+    @current_user = nil # set current user to nil
+  end
 end
