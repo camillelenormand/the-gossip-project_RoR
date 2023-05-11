fa186717f71d893bc9dd84b63547cd1cb308abec
Camille Lenormand <camille.lenormand02@gmail.com>
2023-05-11 22:38:22 +0200

    fix city_id for gossip

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -14,7 +14,7 @@ class GossipsController < ApplicationController
   end
 
   def create 
-    @gossip = Gossip.new(title: params[:title], content: params[:content], user_id: current_user.id)
+    @gossip = Gossip.new(title: params[:title], content: params[:content], user_id: current_user.id, city_id: current_user.city_id)
       
       if @gossip.save
         redirect_to gossips_path, success: "Gossip successfully created !"
