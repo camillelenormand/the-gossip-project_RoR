0d34f4052202bebdc5d1e3f31de03b53fd8ed142
Camille Lenormand <camille.lenormand02@gmail.com>
2023-05-11 09:30:26 +0200

    add password and username to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :sent_private_messages, foreign_key: "sender_id"
   has_many :recipients
-  has_many :received_private_messages, through: :recipients, source: :private_message, dependent: :destroy
-  has_many :comments, dependent: :destroy
-  has_many :likes, dependent: :destroy
+  has_many :received_private_messages, through: :recipients, source: :private_message, dependent: :delete_all
+  has_many :comments, dependent: :delete_all
+  has_many :likes, dependent: :delete_all
+  
+  belongs_to :city, required: false
+  
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Email address invalid" }
+  validates_uniqueness_of :email, :case_sensitive => false
+  validates :password, presence: true, length: { minimum: 6 }
 end
